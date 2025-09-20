5d64d041b89e2c482fa78fd329e2014a95989032
Júlio Tetsuji Pinheiro So <so.julio@hotmail.com>
2025-09-20 10:32:49 -0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Leads from "./pages/leads.tsx";
+import OpportunitiesPage from "./pages/opportunities.tsx";
 
 function App() {
   return (
-    <>
-      <Leads />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/leads" element={<Leads />} />
+        <Route path="/opportunities" element={<OpportunitiesPage />} />
+        <Route path="/" element={<Navigate to="/leads" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/opportunities.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OpportunitiesPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Opportunities</h1>
+      {/* Add your opportunities content here */}
+    </div>
+  );
+};
+
+export default OpportunitiesPage;
